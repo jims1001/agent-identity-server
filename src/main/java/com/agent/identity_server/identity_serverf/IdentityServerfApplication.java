71056543e5f7a706b554a2dd83b0641113b9ccca
jed <166913797+jims1001@users.noreply.github.com>
2025-06-13 16:09:34 +0400

    init project

---
+++ src/main/java/com/agent/identity_server/identity_serverf/IdentityServerfApplication.java
@@ -0,0 +1,13 @@
+package com.agent.identity_server.identity_serverf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServerfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agent/identity_server/identity_serverf/IdentityServerfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent.identity_server.identity_serverf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServerfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
